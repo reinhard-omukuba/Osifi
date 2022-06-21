b91c7b8fd6d88abe781b9f6b3d21939dc9fead5e
reinhard-omukuba <reinhardomukuba@gmail.com>
2022-06-21 12:30:08 +0300

    user levels

---
+++ js/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDBm_-RWQXIkBZONwTtwfxY-_IXQrKGQXc",
+    authDomain: "ofisi-d7510.firebaseapp.com",
+    projectId: "ofisi-d7510",
+    storageBucket: "ofisi-d7510.appspot.com",
+    messagingSenderId: "958308000543",
+    appId: "1:958308000543:web:e779fd4c33991e6b1ac62a",
+    measurementId: "G-97MHJZ24H5"
+  };
+
+
+  firebase.initializeApp(firebaseConfig);
