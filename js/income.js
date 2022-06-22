4ea249643e719c3ced2bbc2dce4452cb4bf62fcb
reinhard-omukuba <reinhardomukuba@gmail.com>
2022-06-22 11:50:17 +0300

    new menus

---
+++ js/income.js
@@ -0,0 +1,33 @@
+
+//EXPENSE
+// Amount
+// Payment method
+// Expense accout - General expense, petty chash, Bills
+// Authourised by
+// Date
+// Expense description
+
+
+// Inventory
+// Item name
+// Item Code (HM4-6464-2022)
+// Item description
+
+// Vendors
+// Vendor name
+// Vendor contact
+
+
+// Employees
+// Employee Name
+// Id number
+// Date of birth
+// contract type -permanent -contact - casual
+// Department
+// Shift - normal 9 - 5, night shift
+// Salary
+
+// Bills
+// Name
+// Amount
+// due date
